3d749303bea573a76fd721b8a73e409260b7fde7
kacp-k01 <kacpkuc@gmail.com>
2025-03-28 12:52:16 +0100

    zad 2 add

--- app_zad2.py
+++ app_zad2.py
@@ -6,14 +6,15 @@ import pathlib
 
 startTime = datetime.now()
 
-# Wczytanie modelu
 filename = pathlib.Path("model.sv")
 with open(filename, "rb") as file:
     model = pickle.load(file)
 
-# Słowniki pomocnicze do wyświetlania opcji
 sex_d = {0: "Kobieta", 1: "Mężczyzna"}
+cp_d = {0: "ASY", 1: "ATA", 2: "NAP", 3: "TA"}
+ecg_d = {0: "LVH", 1: "Normal", 2: "ST"}
 angina_d = {0: "Nie", 1: "Tak"}
+slope_d = {0: "Down", 1: "Flat", 2: "Up"}
 
 def main():
     st.set_page_config(page_title="Predykcja Choroby Serca")
@@ -27,54 +28,31 @@ def main():
         st.title("Czy masz ryzyko choroby serca?")
 
     with left:
-        age = st.slider("Wiek", min_value=18, max_value=100, value=40)
-        sex = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
-        resting_bp = st.slider("Ciśnienie krwi (RestingBP)", min_value=80, max_value=200, value=120)
-        cholesterol = st.slider("Cholesterol", min_value=100, max_value=600, value=200)
-        fasting_bs = st.radio("Cukier na czczo > 120 mg/dl", [0, 1], format_func=lambda x: "Tak" if x == 1 else "Nie")
+        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        cp_radio = st.radio("Typ bólu w klatce piersiowej", list(cp_d.keys()), format_func=lambda x: cp_d[x])
+        ecg_radio = st.radio("Wynik EKG", list(ecg_d.keys()), format_func=lambda x: ecg_d[x])
+        angina_radio = st.radio("Ból wieńcowy po wysiłku", list(angina_d.keys()), format_func=lambda x: angina_d[x])
+        slope_radio = st.radio("Nachylenie ST", list(slope_d.keys()), format_func=lambda x: slope_d[x])
 
     with right:
-        max_hr = st.slider("Maksymalne tętno", min_value=60, max_value=210, value=150)
-        oldpeak = st.slider("Oldpeak (ST depression)", min_value=0.0, max_value=7.0, step=0.1, value=1.0)
-        exercise_angina = st.radio("Ból wieńcowy po wysiłku", list(angina_d.keys()), format_func=lambda x: angina_d[x])
+        age = st.slider("Wiek", min_value=18, max_value=100, value=40)
+        resting_bp = st.slider("Ciśnienie spoczynkowe (RestingBP)", min_value=80, max_value=200, value=120)
+        cholesterol = st.slider("Cholesterol", min_value=100, max_value=600, value=200)
+        fasting_bs = st.radio("Cukier na czczo > 120 mg/dl", [0, 1], format_func=lambda x: "Tak" if x == 1 else "Nie")
+        max_hr = st.slider("Maksymalne tętno (MaxHR)", min_value=60, max_value=210, value=150)
+        oldpeak = st.slider("Oldpeak (obniżenie ST)", min_value=0.0, max_value=7.0, step=0.1, value=1.0)
 
-        # Kategorie zakodowane jako one-hot: ChestPainType, RestingECG, ST_Slope
-        chest_pain = st.selectbox("Typ bólu w klatce", ["ASY", "ATA", "NAP", "TA"])
-        ecg = st.selectbox("Wynik EKG", ["Normal", "ST", "LVH"])
-        st_slope = st.selectbox("Nachylenie ST", ["Up", "Flat", "Down"])
 
     # One-hot encoding dla kategorii
-    chest_pain_vals = [0, 0, 0, 0]
-    if chest_pain == "ASY":
-        chest_pain_vals[0] = 1
-    elif chest_pain == "ATA":
-        chest_pain_vals[1] = 1
-    elif chest_pain == "NAP":
-        chest_pain_vals[2] = 1
-    elif chest_pain == "TA":
-        chest_pain_vals[3] = 1
-
-    ecg_vals = [0, 0, 0]
-    if ecg == "LVH":
-        ecg_vals[0] = 1
-    elif ecg == "Normal":
-        ecg_vals[1] = 1
-    elif ecg == "ST":
-        ecg_vals[2] = 1
-
-    st_vals = [0, 0, 0]
-    if st_slope == "Down":
-        st_vals[0] = 1
-    elif st_slope == "Flat":
-        st_vals[1] = 1
-    elif st_slope == "Up":
-        st_vals[2] = 1
+    cp_vals = [1 if cp_radio == i else 0 for i in range(4)]
+    ecg_vals = [1 if ecg_radio == i else 0 for i in range(3)]
+    slope_vals = [1 if slope_radio == i else 0 for i in range(3)]
 
     # Finalna tablica danych do predykcji
     data = [[
-        age, sex, resting_bp, cholesterol, fasting_bs, max_hr,
-        exercise_angina, oldpeak,
-        *chest_pain_vals, *ecg_vals, *st_vals
+        age, sex_radio, resting_bp, cholesterol, fasting_bs, max_hr,
+        angina_radio, oldpeak,
+        *cp_vals, *ecg_vals, *slope_vals
     ]]
 
     prediction_result = model.predict(data)
