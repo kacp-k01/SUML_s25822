adeba27d3d1e4c3ac9445c376863a879686514e4
kacp-k01 <kacpkuc@gmail.com>
2025-03-28 12:46:43 +0100

    zad 1 add

--- app_zad2.py
+++ app_zad2.py
@@ -1,55 +1,90 @@
-# źródło danych [https://www.kaggle.com/c/titanic/](https://www.kaggle.com/c/titanic)
 
 import streamlit as st
 import pickle
 from datetime import datetime
-startTime = datetime.now()
-# import znanych nam bibliotek
-
 import pathlib
-from pathlib import Path
 
+startTime = datetime.now()
+
+# Wczytanie modelu
 filename = pathlib.Path("model.sv")
+with open(filename, "rb") as file:
+    model = pickle.load(file)
 
-model = pickle.load(open(filename,'rb'))
-# otwieramy wcześniej wytrenowany model
-
-pclass_d = {0:"Pierwsza",1:"Druga", 2:"Trzecia"}
-embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
+# Słowniki pomocnicze do wyświetlania opcji
 sex_d = {0: "Kobieta", 1: "Mężczyzna"}
-# o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
+angina_d = {0: "Nie", 1: "Tak"}
 
 def main():
+    st.set_page_config(page_title="Predykcja Choroby Serca")
+    overview = st.container()
+    left, right = st.columns(2)
+    prediction = st.container()
 
-	st.set_page_config(page_title="Szansa na przeżycie na Titanicu")
-	overview = st.container()
-	left, right = st.columns(2)
-	prediction = st.container()
+    st.image("https://upload.wikimedia.org/wikipedia/commons/6/6a/Coronary_artery_disease.png")
 
-	st.image("https://upload.wikimedia.org/wikipedia/commons/f/fd/RMS_Titanic_3.jpg")
+    with overview:
+        st.title("Czy masz ryzyko choroby serca?")
 
-	with overview:
-		st.title("Czy przeżyjesz katastrofę Titanica?")
+    with left:
+        age = st.slider("Wiek", min_value=18, max_value=100, value=40)
+        sex = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        resting_bp = st.slider("Ciśnienie krwi (RestingBP)", min_value=80, max_value=200, value=120)
+        cholesterol = st.slider("Cholesterol", min_value=100, max_value=600, value=200)
+        fasting_bs = st.radio("Cukier na czczo > 120 mg/dl", [0, 1], format_func=lambda x: "Tak" if x == 1 else "Nie")
 
-	with left:
-		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
-		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
-		pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+    with right:
+        max_hr = st.slider("Maksymalne tętno", min_value=60, max_value=210, value=150)
+        oldpeak = st.slider("Oldpeak (ST depression)", min_value=0.0, max_value=7.0, step=0.1, value=1.0)
+        exercise_angina = st.radio("Ból wieńcowy po wysiłku", list(angina_d.keys()), format_func=lambda x: angina_d[x])
 
-	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=100)
-		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
-		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
-		fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=1)
+        # Kategorie zakodowane jako one-hot: ChestPainType, RestingECG, ST_Slope
+        chest_pain = st.selectbox("Typ bólu w klatce", ["ASY", "ATA", "NAP", "TA"])
+        ecg = st.selectbox("Wynik EKG", ["Normal", "ST", "LVH"])
+        st_slope = st.selectbox("Nachylenie ST", ["Up", "Flat", "Down"])
 
-	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
-	survival = model.predict(data)
-	s_confidence = model.predict_proba(data)
+    # One-hot encoding dla kategorii
+    chest_pain_vals = [0, 0, 0, 0]
+    if chest_pain == "ASY":
+        chest_pain_vals[0] = 1
+    elif chest_pain == "ATA":
+        chest_pain_vals[1] = 1
+    elif chest_pain == "NAP":
+        chest_pain_vals[2] = 1
+    elif chest_pain == "TA":
+        chest_pain_vals[3] = 1
 
-	with prediction:
-		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+    ecg_vals = [0, 0, 0]
+    if ecg == "LVH":
+        ecg_vals[0] = 1
+    elif ecg == "Normal":
+        ecg_vals[1] = 1
+    elif ecg == "ST":
+        ecg_vals[2] = 1
+
+    st_vals = [0, 0, 0]
+    if st_slope == "Down":
+        st_vals[0] = 1
+    elif st_slope == "Flat":
+        st_vals[1] = 1
+    elif st_slope == "Up":
+        st_vals[2] = 1
+
+    # Finalna tablica danych do predykcji
+    data = [[
+        age, sex, resting_bp, cholesterol, fasting_bs, max_hr,
+        exercise_angina, oldpeak,
+        *chest_pain_vals, *ecg_vals, *st_vals
+    ]]
+
+    prediction_result = model.predict(data)
+    prediction_proba = model.predict_proba(data)
+
+    with prediction:
+        st.subheader("Czy ta osoba ma chorobę serca?")
+        st.subheader("Tak" if prediction_result[0] == 1 else "Nie")
+        st.write("Pewność predykcji: {0:.2f}%".format(prediction_proba[0][prediction_result[0]] * 100))
+
 
 if __name__ == "__main__":
     main()
