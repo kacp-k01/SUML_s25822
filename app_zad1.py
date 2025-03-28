ed208db268dd5ae5261c2eb028d40f7f244dadea
kacp-k01 <kacpkuc@gmail.com>
2025-03-28 12:08:41 +0100

    zad 1 add

--- app_zad1.py
+++ app_zad1.py
@@ -9,10 +9,8 @@ startTime = datetime.now()
 import pathlib
 from pathlib import Path
 
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+filename = pathlib.Path("model.sv")
 
-filename = "model.sv"
 model = pickle.load(open(filename,'rb'))
 # otwieramy wcześniej wytrenowany model
 
